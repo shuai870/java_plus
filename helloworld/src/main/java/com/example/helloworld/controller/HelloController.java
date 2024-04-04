3c117f164e1d58d019e3f5c58945df5831dedde6
dongxiaoshuai <894121230@qq.com>
2024-04-04 11:49:20 +0800

    服务器代码

---
+++ helloworld/src/main/java/com/example/helloworld/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+        @RequestMapping(value="/hello",method = RequestMethod.GET)
+        public String hello(){
+            return "string";
+        }
+}
